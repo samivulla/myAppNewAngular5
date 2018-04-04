6c12e557d81147fcfeda1fc4613f0a96692962b3
ramakrishna.k <vempalli.subhan33@gmail.com>
2018-04-04 12:12:42 +0530

    here my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { MyserviceService } from './myservice.service';
+import { ITests, People } from './test';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public testArry: any = [];
+  public test2Arry: any = [];
+  private person: any = [];
+  constructor(private _service: MyserviceService) { }
+
+  ngOnInit() {
+    this._service.getData()
+      .subscribe(data => {
+        this.testArry = data
+        console.log(this.testArry);
+      });
+    this._service.getAllData()
+      .subscribe(data => {
+        this.test2Arry = data
+        console.log(this.test2Arry);
+      })
+    this._service.getReverseData()
+      .subscribe(data => {
+        this.person = data
+        console.log(this.person);
+      });
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { MyserviceService } from './myservice.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './/app-routing.module';
+import { AboutComponent } from './about/about.component';
+import { SoftdevelpComponent } from './softdevelp/softdevelp.component';
+// import { WebdesignComponent } from './webdesign/webdesign.component';
+import { MobileappComponent } from './mobileapp/mobileapp.component';
+// import { SoftwareTestingComponent } from './software-testing/software-testing.component';
+// import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SoftdevelpComponent,
+    // WebdesignComponent,
+    MobileappComponent,
+    // SoftwareTestingComponent,
+    // LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,  
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [MyserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
